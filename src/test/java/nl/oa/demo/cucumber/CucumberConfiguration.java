085bbea9ed9f0df19d87997347388ecde9d9fa12
M.H. de Kwant <m.de.kwant@gmail.com>
2015-07-20 13:17:37 +0200

    working feature

---
+++ src/test/java/nl/oa/demo/cucumber/CucumberConfiguration.java
@@ -0,0 +1,7 @@
+package nl.oa.demo.cucumber;
+
+/**
+ * Created by marcdekwant on 20/07/15.
+ */
+public class CucumberConfiguration {
+}
---
+++ src/test/java/nl/oa/demo/cucumber/service/AppleService.java
@@ -0,0 +1,7 @@
+package nl.oa.demo.cucumber.service;
+
+/**
+ * Created by marcdekwant on 20/07/15.
+ */
+public class AppleService {
+}
